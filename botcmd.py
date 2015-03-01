ad48a0695ba49eddd95ae3f046c16c66cfa62f16
Juhani Haverinen <juhani.haverinen@gmail.com>
2015-03-01 02:30:59 +0200

    Cleaned up the code

--- botcmd.py
+++ botcmd.py
@@ -4,113 +4,163 @@ import random
 import re
 import time
 
-concmd=['/q','/lt','/st','/lg']
+concmd=['/q', '/lt', '/st', '/lg']
 
-blacklist=['bslsk05']
+blacklist = ['bslsk05']
 
-doctor=eliza.eliza()
-trusted=[]
-trustedlock=threading.Lock()
-gods=[]
-godslock=threading.Lock()
-msgs={}
-msglock=threading.Lock()
-authcheck={}
-authchecklock=threading.Lock()
+doctor = eliza.eliza()
+
+trusted = []
+trustedlock = threading.Lock()
+gods = []
+godslock = threading.Lock()
+
+msgs = {}
+msgslock = threading.Lock()
+
+authcheck = {}
+authchecklock = threading.Lock()
 
 die_expr=re.compile("#[0-9]*d([0-9]+|%)")
 
 class Cron(threading.Thread):
 	def __init__(self):
-		self.timedjobs=[]
-		self.timedjobslock=threading.Lock()
-		self.cronctrl=[]
-		self.cronctrllock=threading.Lock()
+		self.timedjobs = []
+		self.timedjobslock = threading.Lock()
+		self.cronctrl = []
+		self.cronctrllock = threading.Lock()
 		threading.Thread.__init__(self)
+	
 	def queuejob(self, time, fn):
 		self.timedjobslock.acquire()
 		self.timedjobs.append((time, fn))
 		self.timedjobslock.release()
+	
 	def ctrl(self, cmd):
 		self.cronctrllock.acquire()
 		self.cronctrl.append(cmd)
 		self.cronctrllock.release()
+	
 	def run(self):
-		run=True
+		run = True
 		while run:
 			time.sleep(1) # Accuracy doesn't need to be high
 			
 			self.cronctrllock.acquire()
 			for cmd in self.cronctrl:
-				if cmd=='QUIT':
-					run=False
+				if cmd == 'QUIT':
+					run = False
 			self.cronctrl=[]
 			self.cronctrllock.release()
 			
 			self.timedjobslock.acquire()
-			self.timedjobs=map((lambda (time,fn): (time-1,fn)), self.timedjobs)
-			torun=map((lambda (time,fn): fn), filter((lambda (time,fn): time<=0), self.timedjobs))
-			self.timedjobs=filter((lambda (time,fn): time>0), self.timedjobs)
+			self.timedjobs = map((lambda (time, fn): (time-1, fn)), self.timedjobs)
+			torun = map((lambda (time, fn): fn), filter((lambda (time, fn): time<=0), self.timedjobs))
+			self.timedjobs = filter((lambda (time, fn): time>0), self.timedjobs)
 			self.timedjobslock.release()
 			
 			for fn in torun:
 				fn()
 
-msglock.acquire()
-f=open('msgs.txt','r')
-for line in f:
-	while len(line)>0 and line[-1]=='\n': line=line[:-1]
-	if len(line)>0:
-		receiver,sender,msg=line.split('\t')
-		if receiver not in msgs:
-			msgs[receiver]=[]
-		msgs[receiver].append((sender,msg))
-f.close()
-msglock.release()
-
 cron=Cron()
 cron.start()
 
+def loadmessages():
+	global msgs, msgslock
+	
+	msgslock.acquire()
+	f = open('msgs.txt', 'r')
+	
+	for line in f:
+		while len(line) > 0 and line[-1] == '\n':
+			line = line[:-1]
+		if len(line) > 0:
+			receiver, sender, msg = line.split('\t')
+			if receiver not in msgs:
+				msgs[receiver] = []
+			msgs[receiver].append((sender, msg))
+	
+	f.close()
+	msgslock.release()
+
+def savemessages():
+	global msgs, msgslock
+	
+	msgslock.acquire()
+	f=open('msgs.txt', 'w')
+	
+	for receiver in msgs:
+		for sender, msg in msgs[receiver]:
+				f.write('%s\t%s\t%s\n' % (receiver, sender, msg))
+	
+	f.close()
+	msgslock.release()
+
+loadmessages()
+
 def addtrusted(nick):
+	global trusted, trustedlock
+	
 	trustedlock.acquire()
+	
 	if nick not in trusted:
 		trusted.append(nick)
+	
 	trustedlock.release()
 
 def rmtrusted(nick):
+	global trusted, trustedlock
+	
 	trustedlock.acquire()
+	
 	if nick in trusted:
 		trusted.remove(nick)
+	
 	trustedlock.release()
 
 def loadtrusted():
+	global trusted, trustedlock
+	
 	trustedlock.acquire()
-	while len(trusted)>0: trusted.pop() #I'm really sorry but trusted=[] created trusted as local variable
+	trusted = []
 	trustedlock.release()
-	f=open('trusted.txt','r')
+	
+	f=open('trusted.txt', 'r')
+	
 	for line in f:
-		while len(line)>0 and line[-1]=='\n': line=line[:-1]
-		if len(line)>0:
+		while len(line) > 0 and line[-1] == '\n':
+			line = line[:-1]
+		if len(line) > 0:
 			addtrusted(line)
+	
 	f.close()
 
 def loadgods():
+	global gods, godslock
+	
 	godslock.acquire()
-	while len(gods)>0: gods.pop() #See above
-	f=open('gods.txt','r')
+	gods = []
+	f=open('gods.txt', 'r')
+	
 	for line in f:
-		while len(line)>0 and line[-1]=='\n': line=line[:-1]
-		if len(line)>0:
+		while len(line) > 0 and line[-1] == '\n':
+			line = line[:-1]
+		if len(line) > 0:
 			gods.append(line)
 			addtrusted(line)
+	
 	f.close()
 	godslock.release()
 
 def savetrusted():
+	global trusted, trustedlock
+	
 	trustedlock.acquire()
-	f=open('trusted.txt','w')
+	f=open('trusted.txt', 'w')
+	
 	for i in trusted:
 		f.write(i+'\n')
+	
 	f.close
 	trustedlock.release()
 	
@@ -120,11 +170,11 @@ loadgods()
 def chmode(irc, chan, nick, mode, args):
 	if args == ['']:
 		if isauthorized(irc, nick):
-			irc.send('MODE %s %s %s'%(chan,mode,nick))
+			irc.send('MODE %s %s %s' % (chan, mode, nick))
 	else:
 		for name in args:
 			if isauthorized(irc, nick):
-				irc.send('MODE %s %s %s'%(chan,mode,name))
+				irc.send('MODE %s %s %s' % (chan, mode, name))
 
 def istrusted(nick):
 	trustedlock.acquire()
@@ -137,27 +187,32 @@ def istrusted(nick):
 
 def initauthcheck(nick):
 	global authcheck, authchecklock
+	
 	authchecklock.acquire()
-	authcheck[nick]=None
+	authcheck[nick] = None
 	authchecklock.release()
 
 def setauthcheckstate(nick, state):
 	global authcheck, authchecklock
+	
 	authchecklock.acquire()
 	if nick in authcheck:
-		authcheck[nick]=state
+		authcheck[nick] = state
 	authchecklock.release()
 
 def getauthcheckstate(nick):
 	global authcheck, authchecklock
+	
 	authchecklock.acquire()
 	if nick in authcheck:
-		state=authcheck[nick]
+		state = authcheck[nick]
 	authchecklock.release()
+	
 	return state
 
 def removeauthcheck(nick):
 	global authcheck, authchecklock
+	
 	authchecklock.acquire()
 	if nick in authcheck:
 		del authcheck[nick]
@@ -168,12 +223,12 @@ def isauthorized(irc, nick):
 		return False
 	
 	initauthcheck(nick)
-	irc.msg('NickServ', 'acc '+nick)
+	irc.msg('NickServ', 'acc ' + nick)
 	cron.queuejob(5, (lambda : setauthcheckstate(nick, False)))
 	
-	state=None
-	while state==None:
-		state=getauthcheckstate(nick)
+	state = None
+	while state == None:
+		state = getauthcheckstate(nick)
 		time.sleep(0.1)
 	removeauthcheck(nick)
 	
@@ -183,7 +238,7 @@ class ArgsfmtError(Exception):
 	def __init__(self, msg):
 		self.msg = msg
 	def __str__(self):
-		return 'Error with argument format: '+msg
+		return 'Error with argument format: ' + msg
 
 ARG_STD = 0
 ARG_OPT = 1
@@ -293,10 +348,15 @@ def parsecmd(line, args):
 	else:
 		return out
 	
-def parse((line,irc)):
-	line=line.split(' ')
-	nick=line[0].split('!')[0][1:]
-	chan=line[2] if line[2][0]=='#' else nick
+def parse((line, irc)):
+	global blacklist
+	global msgs, msgslock
+	global trusted, trustedlock, gods, godslock
+	global doctor, die_expr
+	
+	line = line.split(' ')
+	nick = line[0].split('!')[0][1:]
+	chan = line[2] if line[2][0] == '#' else nick
 	
 	zwsp = '\xe2\x80\x8b'
 	
@@ -309,9 +369,9 @@ def parse((line,irc)):
 		cmdline = [line[3][1:]] + line[4:]
 		while '' in cmdline:
 			cmdline.remove('')
+		
 		if matchcmd(cmdline, '#echo'):
 			text = parsecmd(cmdline, '{text}')
-			print text #debg
 			irc.msg(chan, zwsp+text)
 		elif matchcmd(cmdline, '#op'):
 			args = parsecmd(cmdline, '{args}')
@@ -344,11 +404,11 @@ def parse((line,irc)):
 		elif matchcmd(cmdline, '#msg'):
 			if matchcmd(cmdline, '#msg', 'nick {message}'):
 				msgnick, message = parsecmd(cmdline, 'nick {message}')
-				msglock.acquire()
+				msgslock.acquire()
 				if msgnick not in msgs:
 					msgs[msgnick] = []
 				msgs[msgnick].append((nick, message))
-				msglock.release()
+				msgslock.release()
 			else:
 				irc.msg(chan, 'Usage: #msg nick message')
 		elif matchcmd(cmdline, '#trusted?'):
@@ -423,88 +483,90 @@ def parse((line,irc)):
 					irc.msg(chan, '%s (%s)' % (str(result), ', '.join([str(i) for i in rolls])))
 				else:
 					irc.msg(chan, str(result))
-	elif line[1]=='NOTICE' and line[0].split('!')[0]==':NickServ' and  line[4]=='ACC':
-		if line[5]=='3' or line[5]=='2':
+	elif line[1] == 'NOTICE' and line[0].split('!')[0] == ':NickServ' and line[4] == 'ACC':
+		if line[5] == '3' or line[5] == '2':
 			setauthcheckstate(line[3][1:], True)
 		else:
 			setauthcheckstate(line[3][1:], False)
-			if line[5]=='0':
+			if line[5] == '0':
 				irc.msg(line[3][1:], 'Register account with NickServ')
-			elif line[5]=='1':
-				irc.msg(line[3][1:], 'PRIVMSG %s :Identify with NickServ')
+			elif line[5] == '1':
+				irc.msg(line[3][1:], 'Identify with NickServ')
 			else:
-				irc.msg(line[3][1:], 'WTF, NickServ returned %s'+line[5])
-	elif line[1]=='INVITE' and line[2]==irc.nick and line[3][1:] in irc.chan.split(' '):
+				irc.msg(line[3][1:], 'WTF, NickServ returned %s' % line[5])
+	elif line[1] == 'INVITE' and line[2] == irc.nick and line[3][1:] in irc.chan.split(' '):
 		if isauthorized(irc, nick):
-			irc.send('JOIN '+line[3])
-	elif line[1]=='482':
+			irc.send('JOIN ' + line[3])
+	elif line[1] == '482':
 		irc.msg(line[3], 'Not op')
-	#elif line[1]=='332' or line[1]=='TOPIC':
-	#	if line[1]=='332':
-	#		ch=line[3]
-	#		tp=' '.join(line[4:])[1:]
-	#	elif line[1]=='TOPIC':
-	#		ch=line[2]
-	#		tp=' '.join(line[3:])[1:]
-	#	#Do the magic here
 	
-	msglock.acquire()
-	if (line[1]=='PRIVMSG' or line[1]=='JOIN') and nick in msgs:
-		for sender,msg in msgs.pop(nick):
+	msgslock.acquire()
+	if (line[1] == 'PRIVMSG' or line[1] == 'JOIN') and nick in msgs:
+		for sender, msg in msgs.pop(nick):
 			irc.msg(nick, '<%s> %s' % (sender, msg))
-	msglock.release()
+	msgslock.release()
 
 def execcmd(cmdline):
-	if cmdline[0]=='/q':
-		msglock.acquire()
-		f=open('msgs.txt','w')
-		for receiver in msgs:
-			for sender, msg in msgs[receiver]:
-					f.write('%s\t%s\t%s\n'%(receiver,sender,msg))
-		f.close()
-		msglock.release()
+	if cmdline[0] == '/q':
+		savemessages()
 		savetrusted()
 		
 		cron.ctrl('QUIT')
-	elif cmdline[0]=='/lt':
+	elif cmdline[0] == '/lt':
 		loadtrusted()
-	elif cmdline[0]=='/st':
+	elif cmdline[0] == '/st':
 		savetrusted()
-	elif cmdline[0]=='/lg':
+	elif cmdline[0] == '/lg':
 		loadgods()
 
+def usage(cmd, message = True):
+	usage = {'#echo': 'text',
+	         '#op': '[nick]',
+	         '#deop': '[nick]',
+	         '#voice': '[nick]',
+	         '#devoice': '[nick]',
+	         '#kick': 'nick [reason]',
+	         '#src': '',
+	         '#msg': 'nick message',
+	         '#trusted?': 'nick',
+	         '#trust': 'nick',
+	         '#untrust': 'nick',
+	         '#ls-trusted': '',
+	         '#invite': 'nick',
+	         '#help': '[command]'}
+	
+	if cmd in usage:
+		if message:
+			return 'Usage: %s %s' % (cmd, usage[cmd])
+		else:
+			return usage[cmd]
+	else:
+		return None
+
 def help(cmd):
+	helptext = {'#echo': '#echo text back',
+	            '#op': 'give nick or yourself op rights in case you are trusted by oonbotti2 and identified with NickServ',
+	            '#deop': 'remove your/nick\'s op rights',
+	            '#voice': 'give nick or yourself voice in case you are trusted by oonbotti2 and identified with NickServ',
+	            '#devoice': 'remove your or nick\'s voice in case you are trusted by oonbotti2 and identified with NickServ',
+	            '#kick': 'kicks nick with specified reason',
+	            '#src': 'paste a link to oonbotti2\'s git repo',
+	            '#msg': 'send a message to nick',
+	            '#trusted?': 'tell you if nick or yourself is trusted by oonbotti2',
+	            '#trust': 'add nick to trusted list',
+	            '#untrust': 'remove nick from trusted list',
+	            '#ls-trusted': 'list nicks that are trusted. use only in a query',
+	            '#invite': 'invites nick to channel',
+	            '#help': 'give short info of command or list commands'}
+	
 	if cmd=='':
 		return '#echo #op #deop #voice #devoice #kick #src #msg #trusted? #trust #untrust #ls-trusted #invite #help'
-	elif cmd=='#echo':
-		return '#echo text      echo text back'
-	elif cmd=='#op':
-		return '#op [nick]      give nick or yourself op rights in case you are trusted by oonbotti2 and identified with NickServ'
-	elif cmd=='#deop':
-		return '#deop [nick]      remove your/nick\'s op rights'
-	elif cmd=='#voice':
-		return '#voice [nick]      give nick or yourself voice in case you are trusted by oonbotti2 and identified with NickServ'
-	elif cmd=='#devoice':
-		return '#devoice [nick]      remove your or nick\'s voice in case you are trusted by oonbotti2 and identified with NickServ'
-	elif cmd=='#kick':
-		return '#kick nick reason      kicks nick with specified reason'
-	elif cmd=='#src':
-		return '#src      paste a link to oonbotti2\'s git repo'
-	elif cmd=='#msg':
-		return '#msg nick message      send a message to nick'
-	elif cmd=='#trusted?':
-		return '#trusted? [nick]      tell you if nick or yourself is trusted by oonbotti2'
-	elif cmd=='#trust':
-		return '#trust nick      add nick to trusted list'
-	elif cmd=='#untrust':
-		return '#untrust nick      remove nick from trusted list'
-	elif cmd=='#ls-trusted':
-		return '#ls-trusted      list nicks that are trusted. use only in a query'
-	elif cmd=='#invite':
-		return '#invite nick      invites nick to channel'
-	elif cmd=='#help':
-		return '#help [command]      give short info of command or list commands'
 	elif cmd=='me':
 		return 'I shall.'
+	elif cmd in helptext:
+		if helptext[cmd]:
+			return '%s %s      %s' % (cmd, usage(cmd, False), helptext[cmd])
+		else:
+			return '%s %s' % (cmd, usage(cmd, False))
 	else:
 		return None
